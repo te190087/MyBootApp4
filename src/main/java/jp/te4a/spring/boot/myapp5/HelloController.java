aba4e62ab563168dbc8d94e4a650f4ef0f49555b
190087 <190087@e0601-17.JC-21.local>
2022-05-24 11:16:55 +0900

    kadai5

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -1,6 +1,12 @@
 package jp.te4a.spring.boot.myapp5;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HelloController {
